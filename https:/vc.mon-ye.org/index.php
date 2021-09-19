dada09dad1200ee72f3638748490d2cfd1f3c5de
يمن. كوفيدا19 <90987585+ahmedahmeder023@users.noreply.github.com>
2021-09-19 16:41:41 +0300

    Create index.php
    COVID-19 Vaccination Certificate
    شهادة تطعيم كوفيد-19
    Date of Birth	1984	1984	سنة الميلاد
    National ID	7240873	7240873	الرقم الوطني
    Unique ID	2021-06-26-61754	2021-06-26-61754	الرقم في النظام
    Name		علي عبدالرحمن صالح مقرح	الأسم
    Date of Vaccination تاريخ التحصين	Lot No. رقم التشغيلة	Vaccine Type نوع اللقاح	Dose الجرعة
    2021-05-30	4121z010	astrazeneca	DOSE1
    2021-07-25.   4121z010.     astrazeneca. DOSE2

---
+++ https:/vc.mon-ye.org/index.php
@@ -0,0 +1,10 @@
+
+COVID-19 Vaccination Certificate
+شهادة تطعيم كوفيد-19
+Date of Birth	1984	1984	سنة الميلاد
+National ID	7240873	7240873	الرقم الوطني
+Unique ID	2021-06-26-61754	2021-06-26-61754	الرقم في النظام
+Name		علي عبدالرحمن صالح مقرح	الأسم
+Date of Vaccination تاريخ التحصين	Lot No. رقم التشغيلة	Vaccine Type نوع اللقاح	Dose الجرعة
+2021-05-30	4121z010	astrazeneca	DOSE1
+2021-07-25.   4121z010.     astrazeneca. DOSE2
